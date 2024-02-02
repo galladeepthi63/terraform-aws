45a8ed6a5526e16623ffbdc12427e4890e9180d8
galladeepthi63 <deepthig63@outlook.com>
2024-02-02 23:20:18 +0530

    update access key

--- roboshop-infra-dev/01-vpc/provider.tf
+++ roboshop-infra-dev/01-vpc/provider.tf
@@ -5,7 +5,7 @@ terraform {
       version = "~> 5.0"
     }
   }
-  
+
   # backend "s3" {
   #   bucket = "terraformjenk"
   #   key    = "vpc"
@@ -17,6 +17,7 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"   
-  
+  access_key = "AKIAV3M7G2D26KQE3ECW"
+  secret_key = "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"
 }
 
